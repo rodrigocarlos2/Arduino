94798a9ba0b0415269b5e3936e5630d38ea3541f
rodrigocarlos2 <rodrigo19962010@live.com>
2016-04-08 12:09:26 -0300

    Create Code2.ino

---
+++ Code2.ino
@@ -0,0 +1,16 @@
+
+const int numberConstant = 3;
+
+// numberConstant is a variable of the type CONSTANT, where your value
+// cannot be modified.
+
+int buttonPin = 10;
+
+void setup(){
+  // numberConstant = 4; return ERROR: variable constant cannot be modified!!
+  pinMode(buttonPin, OUTPUT);
+  // Function for configure pins.
+}
+
+void loop(){
+}

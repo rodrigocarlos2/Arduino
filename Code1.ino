8de2efb010f7b10bfec43acae505e7ed98966b13
rodrigocarlos2 <rodrigo19962010@live.com>
2016-04-08 12:06:15 -0300

    Create Code1.ino

---
+++ Code1.ino
@@ -0,0 +1,12 @@
+
+// Basic Functions of the Arduino.
+
+void setup(){
+  // Once Execution
+  // Much used for pin modes, begin variables, etc.
+}
+
+void loop(){
+  // Infinit Execution
+  // The execution ends only when the board is off
+}

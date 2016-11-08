e061215c65280645460a9ffc2dfdaa4a6123c315
Rodrigo <rodrigo19962010@live.com>
2016-11-08 15:17:30 -0200

    Show de Luzes incluído com Rodrigo Carlos

---
+++ Show De Luzes.ino
@@ -0,0 +1,44 @@
+
+
+unsigned long tempoAnterior=0;
+
+unsigned long tempoMaximo = 1000;
+
+void setup(){
+
+	pinMode(12, OUTPUT);
+	pinMode(11, OUTPUT);
+	pinMode(10, OUTPUT);
+	Serial.begin(9600);
+
+}
+
+void loop(){
+
+	unsigned long tempoAtual = millis();
+	// Função para ler o tempo atual
+
+	boolean ledRed, ledOra, ledWhi;
+
+	ledRed = digitalRead(12);
+	ledOra = digitalRead(11);
+	ledWhi = digitalRead(10);
+
+	if(ledRed==true){
+		digitalWrite(12, LOW);
+		digitalWrite(11, HIGH);
+	}
+	else if(ledOra==true){
+		digitalWrite(11, LOW);
+		digitalWrite(10, HIGH);
+	}
+	else{
+		digitalWrite(10, LOW);
+		digitalWrite(12, HIGH);
+	}
+
+	while(tempoAtual-tempoAnterior<tempoMaximo){
+		tempoAtual = millis();
+	}
+
+}

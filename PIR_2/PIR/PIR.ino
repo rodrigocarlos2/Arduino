854be360d90834839e42a35ec75ae9a17e7af747
Rodrigo <rodrigo19962010@live.com>
2016-12-17 13:37:19 -0300

    PIR

--- PIR_2/PIR/PIR.ino
+++ PIR_2/PIR/PIR.ino
@@ -1,13 +1,17 @@
 
 int PIR=8;
+// Port of PIR
 
-void setup() {
+void setup(){
   Serial.begin(9600);
+  // Begin of the Serial Communication
 }
 
-void loop() {
+void loop(){
   int action = digitalRead(PIR);
+  // Entrance of PIR
 
+  // Now, it is the action
   if(action){
     Serial.println("Movimento");
   }
